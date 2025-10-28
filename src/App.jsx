7c21490f63ddbd686495c1c400086ea2e3fee8d7
meriemmhd <meriemmehdi{~@gmail.com>
2025-10-28 15:14:13 +0100

    btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { AffDesc } from './components/AffDesc.jsx';
 
 function App() {
   const [value, setValue] = useState([])
+  const [min,setmin]=useState(130);
+  const [loading, setLoading] = useState(true);
   const paris = async () =>{
     const res = await fetch('https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/records?limit=20')
     const data = await res.json()
-    console.log(data)
-
-    setValue(data.results)
+    console.log(data);
+    setValue(data.results);
+     setLoading(false);
   }
   useEffect(() =>{
   paris()
@@ -17,6 +20,7 @@ function App() {
 
   return (
     <>
+<<<<<<< HEAD
     <ul>
     {value.map((item) =>(
 
@@ -29,8 +33,10 @@ function App() {
       ))}
   
   </ul>
+=======
+    <AffDesc value={value} setValue={setValue} min={min} setmin={setmin} />
+>>>>>>> 6b50058 (mise a jour)
     </>
-  )
-}
+)}
 
 export default App
